5ebe0dd406c67484e55798e27ce859a8cd22279a
Serhii Zelinskyi <zelinskyi.serh@gmail.com>
2024-02-25 13:38:59 +0200

    implemented signup login and seperate auth logic to different components

--- src/app/employee/create/page.tsx
+++ src/app/employee/create/page.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { useSearchParams } from 'next/navigation'
 import { ChangeEvent, useState } from "react";
 import { useAppDispatch, useAppSelector } from "@/GlobalRedux/hooks";
